a330df604ab1f32cd45e8a056f8899909bf9985e
Iain Sheerin <iain.sheerin@gmail.com>
2020-09-16 22:18:10 -0400

    Default files for iOS app
    Create git repo from Xcode.

---
+++ HackAThing/HackAThingApp.swift
@@ -0,0 +1,18 @@
+//
+//  HackAThingApp.swift
+//  HackAThing
+//
+//  Created by Iain Sheerin on 9/16/20.
+//
+
+import SwiftUI
+//Test
+
+@main
+struct HackAThingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
