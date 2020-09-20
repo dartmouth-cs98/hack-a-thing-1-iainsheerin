9e58eb6ad76c97d91147ae7c48a7e78728a43e54
Iain Sheerin <iain.sheerin@gmail.com>
2020-09-20 16:48:56 -0400

    Add list and favorite ability for workouts

---
+++ HackAThing/WorkoutDetail.swift
@@ -0,0 +1,20 @@
+//
+//  WorkoutDetail.swift
+//  HackAThing
+//
+//  Created by Iain Sheerin on 9/20/20.
+//
+
+import SwiftUI
+
+struct WorkoutDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WorkoutDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        WorkoutDetail()
+    }
+}
---
+++ HackAThing/WorkoutList.swift
@@ -0,0 +1,20 @@
+//
+//  WorkoutList.swift
+//  HackAThing
+//
+//  Created by Iain Sheerin on 9/20/20.
+//
+
+import SwiftUI
+
+struct WorkoutList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WorkoutList_Previews: PreviewProvider {
+    static var previews: some View {
+        WorkoutList()
+    }
+}
