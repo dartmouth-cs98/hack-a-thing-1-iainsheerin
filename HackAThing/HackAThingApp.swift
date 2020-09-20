0ce98ebc2869e21acda80f43794c672a663938b2
Iain Sheerin <iain.sheerin@gmail.com>
2020-09-20 09:39:33 -0400

    Main in SwiftUI not Appdelgate and SceneDelegate

--- HackAThing/HackAThingApp.swift
+++ HackAThing/HackAThingApp.swift
@@ -5,14 +5,14 @@
 //  Created by Iain Sheerin on 9/16/20.
 //
 
-import SwiftUI
-//Test
-//Test2
-@main
-struct HackAThingApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
-}
+//import SwiftUI
+////Test
+////Test2
+//@main
+//struct HackAThingApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            SceneDelegate()
+//        }
+//    }
+//}
